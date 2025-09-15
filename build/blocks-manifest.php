418bde8c2e0048dec25599df81ebaab8d106989a
Roelof Wobben <r.wobben@home.nl>
2025-09-15 15:45:02 +0200

    first commit

---
+++ build/blocks-manifest.php
@@ -0,0 +1,25 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'my-reading-list' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'my-reading-list/reading-list-block',
+		'version' => '0.0.1',
+		'title' => 'My Reading List Block',
+		'category' => 'widgets',
+		'icon' => 'smiley',
+		'description' => 'Displays a list of books for a reading list.',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'my-reading-list',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'viewScript' => 'file:./view.js'
+	)
+);
---
+++ build/my-reading-list/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '57bba97be775b7024703');
